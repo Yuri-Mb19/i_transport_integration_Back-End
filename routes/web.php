4c6eddc19c17399a15b9c9d0e126597a1d58500b
Yuri Mbalane <Yurimnbalane19@gmail.com>
2025-01-05 19:24:23 +0200

    Login Integration

--- routes/web.php
+++ routes/web.php
@@ -11,15 +11,19 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
+
+*/
 Route::get('/', function () {
     return view('welcome');
 });
-*/
 
-Route::get('/',[\App\Http\Controllers\PrincipalController::class,'Principal'])->name('site.index');
+//Route::get('/',[\App\Http\Controllers\PrincipalController::class,'Principal'])->name('site.index');
 Route::get('/sobre-nos',[\App\Http\Controllers\SobreNosController::class,'sobreNos'])->name('site.sobre-nos');
 Route::get('/contato',[\App\Http\Controllers\ContatoController::class,'contato'])->name('site.contato');
 Route::get('/blog',[\App\Http\Controllers\BlogController::class,'blog'])->name('site.blog');
 Route::get('/airport',[\App\Http\Controllers\AirportTransferController::class,'airport'])->name('site.airport');
 Route::get('/single-service',[\App\Http\Controllers\SingleServiceController::class,'single'])->name('site.single');
 Route::get('/what-our-clients-say',[\App\Http\Controllers\WhatOurClientsController::class,'what'])->name('site.what');
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
